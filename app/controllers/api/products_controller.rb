de30f24ad52016d6346266be8542f26fce3befc8
YOUR NAME <YOUR@EMAIL.com>
2020-10-28 14:30:17 -0500

    adds product1

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,15 @@
 class Api::ProductsController < ApplicationController
   def products_action
-    @product = Product.all
+    @products = Product.all
     render "products.json.jb"
+  end
+
+  def blender
+    @product1 = Product.first
+    render "blender.json.jb"
+  end
+
+  def dutch_oven
+    render "dutch_oven.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/products_path" => "products#products_action"
+    get "/all_products_path" => "products#products_action"
+
+    get "/blender_path" => "products#blender"
+
+    get "/dutch_oven_path" => "products#dutch_oven"
   end
 end
