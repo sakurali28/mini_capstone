d1dfba55921aa4df197d6c4f40ef0e4a16f42c99
YOUR NAME <YOUR@EMAIL.com>
2020-11-10 12:41:34 -0600

    adds jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+
     get "/products" => "products#index"
     get "/products/:show" => "products#show"
     post "/products" => "products#create"
