8f9e6673a4fcf409a142b23dc3c40da98ded65d7
YOUR NAME <YOUR@EMAIL.com>
2020-11-12 13:11:23 -0600

    adds ProductCategory associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,14 +16,8 @@ class Product < ApplicationRecord
   end
 
   belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
   has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end
-
   has_many :orders
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 end
